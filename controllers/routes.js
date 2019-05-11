7e1bde814a77268caf5e200f034c2076095fe320
tyler <tylercedge@gmail.com>
2019-05-11 02:19:05 -0400

    clicking on devour button successfully deleting burger from screen

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,12 +6,21 @@ var router = express.Router();
 var cat = require("../models/burgers.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(burger_data) {
+router.get("/", function (req, res) {
+  burger.all(function (burger_data) {
     console.log(burger_data);
-    res.render("index", {burger_data});
+    res.render("index", {
+      burger_data
+    });
+  })
+})
+
+router.put("/burgers/update", function (req, res) {
+  burger.update(req.body.burger_id, function (result) {
+    console.log(result);
+    res.redirect('/');
   });
-  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- models/burgers.js
+++ models/burgers.js
@@ -1,10 +1,13 @@
 var orm = require('../config/orm.js');
 
 var burger = {
-    all: function(cb) {
-        orm.all('burgers', function(res){
+    all: function (cb) {
+        orm.all('burgers', function (res) {
             cb(res);
         })
+    },
+    update: function (id, cb) {
+        orm.update('burgers', id, cb);
     }
 }
 
